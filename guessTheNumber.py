12a7fc46c0cd99cb7696d86b303eb0592b473f06
Jack Kong <jfn.kong@gmail.com>
2020-04-27 00:47:10 -0500

    firstCommit of guessing game. runs in terminal

---
+++ guessTheNumber.py
@@ -0,0 +1,28 @@
+#This is a number guessing game.
+#You input a number to guess if it matches a randomly selected number between 1 and 20.
+#You will have 6 chances to numG the number.
+import random
+
+print('Hello. What is your name?')
+name = input()
+numToGuess = random.randint(1,20)
+totalChances = 7
+print(name+', guess a number between 1 and 20.')
+
+for numOfGuesses in range(1,totalChances):
+    numG = int(input())
+    while(numG > 20 and numG > 0):
+        print("Sorry, the value you entered is not between 1 and 20. Enter a valid number.")
+        numG = int(input())
+    if numG < numToGuess:
+        print('Your guess is too low. Try again. You have '+str(totalChances-numOfGuesses)+' guesses left.')
+    elif numG > numToGuess:
+        print('Your guess is too high. Try again. You have '+str(totalChances-numOfGuesses)+' guesses left.')
+    else:
+        break
+
+if numG == numToGuess:
+    print('Congratulations '+name+'! You guessed the right number which is '+str(numToGuess)+'.')
+    print('You took '+str(numOfGuesses)+' guesses to find the number.')
+else:
+    print('Sorry! You took too many guesses. The number to guess was '+str(numToGuess)+'.')
